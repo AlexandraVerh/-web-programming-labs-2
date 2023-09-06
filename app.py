59a33a7bec18cab0ac54601b5eb1b77b1c5a4463
Александра Верхозина <aleksandraverhozina@yandex.ru>
2023-09-06 17:34:58 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctupe html>
+<html>
+    <head>
+        <title>Верхозина Александра Андреевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Верхозина Александра, ФБИ-14, 3 курс,2023
+        </footer>
+    </body>
+</html>
+"""
+
+    
