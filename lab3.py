5e050a736dcef1d2488d07de666c398a528b1db1
Верхозина Александра <aleksandraverhozina@yandex.ru>
2023-10-04 18:34:03 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, render_template, redirect, request
 lab3 = Blueprint('lab3',__name__)
 
 @lab3.route("/lab3/")
 def lab():
-    return redirect("/lab3/form1")
+    return render_template('lab3.html')
 
 
 @lab3.route("/lab3/form1")
 def form1():
-    return render_template('lab3.html')
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex)
 
