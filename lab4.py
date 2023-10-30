7169d21ef9e800c10d650e2dee0975c213e43183
Верхозина Александра <aleksandraverhozina@yandex.ru>
2023-10-30 22:24:33 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -29,5 +29,45 @@ def login():
         error  = 'Неверный логин и/или пароль '
         return render_template('login.html', error=error, username=username, password=password)
     
+
+
+@lab4.route('/lab4/holodil', methods=['GET', 'POST'])
+def holodil():
+    if request.method == 'GET':
+        return render_template('holodil.html')
+    
+    temperature = request.form.get('temperature')
+    error = ''
+    message = ''  # Начальное значение для переменной message
+    snowflakes = ''  # Начальное значение для переменной snowflakes
+    
+    if temperature is None or temperature == '':
+        error = 'ошибка: не задана температура'
+        return render_template('holodil.html', error=error, temperature=temperature)
+    
+    if int(temperature) < -12:
+        error = 'не удалось установить температуру — слишком низкое значение'
+        return render_template('holodil.html', error=error, temperature=temperature)
+    
+    if int(temperature) > -1:
+        error = 'не удалось установить температуру — слишком высокое значение'
+        return render_template('holodil.html', error=error, temperature=temperature)
+    
+    if -12 <= int(temperature) <= -9:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️❄️❄️'
+        return render_template('holodil.html', error=error, temperature=temperature, message=message, snowflakes=snowflakes)
+
+    if -8 <= int(temperature) <= -5:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️❄️'
+        return render_template('holodil.html', error=error, temperature=temperature, message=message, snowflakes=snowflakes)
+
+    if -4 <= int(temperature) <= -1:
+        message = f'Установлена температура: {temperature}°C'
+        snowflakes = '❄️'
+        return render_template('holodil.html', error=error, temperature=temperature, message=message, snowflakes=snowflakes)
+
+    
     
    
