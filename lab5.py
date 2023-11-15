d7b454e1b60486d7841c44de2956a371518031e7
Верхозина Александра <aleksandraverhozina@yandex.ru>
2023-11-15 17:46:43 +0700

    Добавление трех пользователей

--- lab5.py
+++ lab5.py
@@ -56,3 +56,70 @@ def users():
     cur.execute("SELECT * FROM users;")
     result = cur.fetchall()
     return render_template('lab5users.html', users=result)
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def registerPage():
+    error = ''
+
+    if request.method == 'GET':
+        return render_template('register.html', error=error)
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not (username and password):
+        error="Пожалуйста, заполните все поля"
+        print(error)
+        return render_template('register.html', error=error)
+
+    
+
+    conn = dbConnect()
+    cur = conn.cursor()
+    cur.execute(f"SELECT username FROM users WHERE username = '{username}';")
+
+    if cur.fetchone() is not None:
+        error = "Пользователь с данным именем уже существует"
+
+        dbClose(cur, conn)
+        return render_template('register.html', error=error)
+    
+    cur.execute(f"INSERT INTO users (username, password) VALUES ('{username}','{password}');")
+    conn.commit()
+    dbClose(cur, conn)
+
+    return redirect("/lab5/login")
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def loginPage():
+    error = ''
+
+    if request.method == 'GET':
+        return render_template('login2.html', error=error)
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not (username and password):
+        error="Пожалуйста, заполните все поля"
+        return render_template('login2.html', error=error)
+
+    conn = dbConnect()
+    cur = conn.cursor()
+    cur.execute(f"SELECT id, password FROM users WHERE username = '{username}'")
+    result = cur.fetchone()
+
+    if result is None:
+        error="Неправильный логин или пароль"
+        conn.close()  # Закрытие соединения
+        return render_template('login2.html', error=error)
+    
+    userID, hashPassword = result
+    if check_password_hash(hashPassword, password):
+        session['id'] = userID
+        session['username'] = username
+        conn.close()  # Закрытие соединения
+        return redirect("/lab5/")
+    else:
+        error=("Неправильный логин или пароль")
+        return render_template("login2.html", errors=errors)
