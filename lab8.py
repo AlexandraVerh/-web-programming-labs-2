31b5826858be18c971e20b8fad993125ee4fa279
Верхозина Александра <aleksandraverhozina@yandex.ru>
2023-12-12 20:58:42 +0700

    Получение информации о курсе/-ах по REST

--- lab8.py
+++ lab8.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, abort
 
 lab8 = Blueprint('lab8',__name__)
 
@@ -6,3 +6,22 @@ lab8 = Blueprint('lab8',__name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "c#", "videos": 8} #если цена не указана, то курс бесплатный
+]
+
+#Здесь мы определяем, что у нас будет специальный путь, по которому будет
+#"жить" API
+#здесь список курсов
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return courses
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)  # возвращаем ошибку 404, если номер выходит за пределы
+    return courses[course_num]
